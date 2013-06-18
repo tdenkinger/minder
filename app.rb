fed4f5b9b48f56c62609fb6fd73cae823c35ac8d
Troy Denkinger <lisa+tdenkinger@gmail.com>
2013-06-18 08:58:16 -0400

    Support for sinatra app reloading.

--- app.rb
+++ app.rb
@@ -1,16 +1,17 @@
 require 'sinatra'
+require 'sinatra/reloader'
 require './lib/Reminder'
 require './lib/DB'
 
 r = Reminder.new Database.new
 
 get '/' do
-  "Nothing to see here."
+  "Nothing to see here, pal."
 end
 
-post "/" do
-  # r.add params[:stuff]
-  p params
+post "/reminder/:reminder" do
+  p params[:reminder]
+  r.add params[:reminder]
 end
 
 get "/reminders" do
