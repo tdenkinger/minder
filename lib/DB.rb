f5368760d3605fc1c02e626c26eadcba0bd9acb7
Troy Denkinger <tdenkinger@gmail.com>
2013-07-09 08:33:21 -0400

    Get messages by tag.

--- lib/DB.rb
+++ lib/DB.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class Database
   attr_reader :items
 
@@ -13,4 +15,28 @@ class Database
     items << reminder
   end
 
+  def messages(tag: nil)
+    messages = []
+    items.each do |i|
+      messages << get_message(tag, i) unless message.empty?
+    end
+    messages
+  end
+
+private
+
+  def get_message(tag, i)
+    item = parse_item(i)
+    message = check_tags(tag, item)
+    messages << message unless message.empty?
+  end
+
+  def parse_item(item)
+    JSON.parse(i)
+  end
+
+  def check_tags(tag, item)
+    return "" unless item["tags"]
+    return "" unless item["tags"].include?(tag)
+  end
 end
--- spec/db_spec.rb
+++ spec/db_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/DB'
+require 'json'
 
 describe Database do
   let(:db){ Database.new }
@@ -8,7 +9,26 @@ describe Database do
   end
 
   it "accepts new reminders" do
-    db.save_reminder "this is a test"
+    db.save_reminder '{
+        "message": "remember milk",
+            "tags": [
+                "testing",
+                "breakfast"
+            ]
+        }'
     expect(db.reminders).to_not be_empty
+  end
+
+  it "returns all messages" do
+    db.save_reminder '{"message": "remember milk"}'
+    db.save_reminder '{"message": "eat your veggies"}'
+    expect(db.messages).to eq ['remember milk', 'eat your veggies']
+  end
+
+  it "returns all messages for a specific tag" do
+    db.save_reminder '{"message": "remember milk", "tags":["store", "breakfast"]}'
+    db.save_reminder '{"message": "eat your veggies", "tags":["lunch"]}'
+    db.save_reminder '{"message": "clean your plate"}'
+    expect(db.messages(:tag => 'store')).to eq ['remember milk']
   end
 end
