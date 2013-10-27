67e75fc3107c15a524e1dbd8dd3f721f1eb91c2d
Troy Denkinger <tdenkinger@gmail.com>
2013-10-27 10:11:45 -0400

    wip: Getting started.

--- lib/reminders.rb
+++ lib/reminders.rb
@@ -1,2 +1,11 @@
+require "json"
+
 class Reminders
+  def self.add_reminder reminder
+    1
+  end
+
+  def self.get_reminders
+    reminders = [{ :id => 1, :reminder => "Number 1" }, {:id => 2, :reminder => "Number 2"}].to_json
+  end
 end
--- spec/reminders_spec.rb
+++ spec/reminders_spec.rb
@@ -4,6 +4,12 @@ require_relative "../lib/reminders"
 describe Reminders do
 
   it "accepts a new reminder" do
+    reminder_id = Reminders.add_reminder :reminder => "Remember the milk"
+    expect(reminder_id).to eql 1
+  end
 
+  it "returns all reminders" do
+    reminders = Reminders.get_reminders
+    # expect(reminders[0][:id]).to eql ""
   end
 end
